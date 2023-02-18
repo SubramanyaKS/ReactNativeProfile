805c9a04ef2a6c1cd13a2533e34a44269b4738f3
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-18 22:46:18 +0530

    updated home

---
+++ components/HomePage.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default function HomePage() {
+
+    return (
+        <View style={styles.container}>
+        <Text style={styles.text}>Hi I am </Text>
+        <Text style={{fontSize:40,color:"#1f51ff"}}>Subramanya K S</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor:"#000",
+      },
+      text:{
+        // fontFamily:"cursive",
+        fontSize:25,
+        color:"#1f51ff",
+      }
+})
